d82e36054c43c7e391432dda80da39747f0daa02
Nicolas White <nw11@illinois.edu>
2018-12-15 22:24:40 -0500

    prep for heroku

--- server/server.js
+++ server/server.js
@@ -276,6 +276,6 @@ socket.on('memes', (memes) => {
 
 app.use('/', indexRoutes);
 
-server.listen(3000)
+//server.listen(3000)
 
-//server.listen(process.env.PORT || 5000)
+server.listen(process.env.PORT || 5000)
